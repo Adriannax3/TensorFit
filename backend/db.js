adaa11e03394237941a1cbef24b2dc2b9eb3c949
Adrianna <115454102+Adriannax3@users.noreply.github.com>
2025-12-08 18:58:40 +0100

    Delete file

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('TensorFit', 'ada', 'freja2137', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
