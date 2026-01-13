d8ccbc89cc5382db91db706cda64b45beebc5e12
Adriannax3 <115454102+Adriannax3@users.noreply.github.com>
2026-01-13 18:06:33 +0100

    Check .env

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const envPath = path.resolve(__dirname, '.env');
+if (!fs.existsSync(envPath)) {
+  console.error("Brak pliku .env! Przykładowy plik znajduje się w .env.example");
+  process.exit(1);
+}
+// check example variable
+const requiredEnv = ["PORT", "JWT_SECRET", "DB_NAME", "DB_HOST", "DB_USER", "DB_PASSWORD", "DB_PORT"];
+for (const key of requiredEnv) {
+  if (!process.env[key]) {
+    console.error(`Brak wymaganej zmiennej środowiskowej: ${key}. Sprawdź .env.example`);
+    process.exit(1);
+  }
+}
+
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./db");
